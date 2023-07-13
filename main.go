56e464a0c86fb498693e258bf314f9c3e382c79a
vinicius <viniciuscarrocine@gmail.com>
2023-07-13 11:58:08 -0300

    Ajustes finais na conexão, primeiro commit não pegou todas as alterações

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -18,7 +19,7 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func DataBaseConect() *sql.DB {
-	conexao := "username=vinicius dbname=alura_loja password=senior host=localhost sslmode=disable"
+	conexao := "user=vinicius dbname=alura_loja password=senior host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 	if err != nil {
 		panic(err.Error())
@@ -27,15 +28,36 @@ func DataBaseConect() *sql.DB {
 }
 
 func main() {
+	db := DataBaseConect()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{"Tenis", "Confortavel", 89, 120},
-		{"Computador", "Bem rápido", 3999, 10},
-		{"Celular", "Resistênte", 299, 50},
+	db := DataBaseConect()
+	selection, err := db.Query("SELECT * FROM Produtos")
+	if err != nil {
+		panic(err.Error())
 	}
-	temp.ExecuteTemplate(w, "Index", produtos)
+
+	p := Produto{}
+	products := []Produto{}
+
+	for selection.Next() {
+		err := selection.Scan(
+			&p.Id,
+			&p.Nome,
+			&p.Descricao,
+			&p.Preco,
+			&p.Quantidade,
+		)
+		if err != nil {
+			panic(err.Error())
+		}
+		products = append(products, p)
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
+	defer db.Close()
 }
